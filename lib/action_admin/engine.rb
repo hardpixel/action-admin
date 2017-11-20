4066f6975a4e9f084698c6bc0aec427bfabb6080
Jonian Guveli <jonian@hardpixel.eu>
2017-11-20 17:14:18 +0200

    update partials

--- lib/action_admin/engine.rb
+++ lib/action_admin/engine.rb
@@ -1,4 +1,11 @@
 module ActionAdmin
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.template_engine :slim
+    end
+
+    initializer "action_admin.assets.precompile" do |app|
+      app.config.assets.precompile += %w(admin/*)
+    end
   end
 end
