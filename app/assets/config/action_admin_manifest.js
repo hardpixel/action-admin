6768ce1ec2a9637f4b7e4eb49b49a62239364939
Jonian Guveli <jonian@hardpixel.eu>
2017-11-24 09:43:14 +0200

    remove unneeded files

--- app/assets/config/action_admin_manifest.js
+++
@@ -1,2 +0,0 @@
-//= link_directory ../javascripts/action_admin .js
-//= link_directory ../stylesheets/action_admin .css
--- app/jobs/action_admin/application_job.rb
+++
@@ -1,4 +0,0 @@
-module ActionAdmin
-  class ApplicationJob < ActiveJob::Base
-  end
-end
