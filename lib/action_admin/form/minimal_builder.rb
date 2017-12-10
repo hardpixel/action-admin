591c025e7b8873d1f6d5eaa72a8e14fff74e48c2
Jonian Guveli <jonian@hardpixel.eu>
2017-12-10 14:11:45 +0200

    optimize minimal form builder

--- lib/action_admin/form/minimal_builder.rb
+++ lib/action_admin/form/minimal_builder.rb
@@ -3,14 +3,13 @@ module ActionAdmin
     class MinimalBuilder < Builder
       # Form builder input
       def input(attribute_name, options = {}, &block)
-        if options[:placeholder].nil?
-          options[:placeholder] ||= if object.class.respond_to?(:human_attribute_name)
-            object.class.human_attribute_name(attribute_name.to_s)
-          else
-            attribute_name.to_s.humanize
-          end
+        options  = @defaults.deep_dup.deep_merge(options) if @defaults
+        input    = find_input(attribute_name, options, &block)
+        excluded = [:radio, :checkbox, :boolean, :check_boxes, :radio_buttons]
 
-          options[:label] = false if options[:label].nil?
+        if !input.input_type.in?(excluded) and options[:placeholder].nil?
+          options[:placeholder] ||= input.send(:raw_label_text)
+          options[:label]       ||= false
         end
 
         super
