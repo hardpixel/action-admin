bb4465a967d5df80fed98a8ab66842617b5a0455
Olibia Tsati <olibia@hardpixel.eu>
2017-12-17 20:44:47 +0200

    update tinymce input

--- app/inputs/action_admin/tinymce_input.rb
+++ app/inputs/action_admin/tinymce_input.rb
@@ -2,7 +2,16 @@ module ActionAdmin
   class TinymceInput < SimpleForm::Inputs::TextInput
     def input(wrapper_options)
       input_html_options[:data] ||= {}
-      input_html_options[:data].merge!({ tiny_mce_editor: '', content_css: template.asset_path('admin/tinymce-editor.css') })
+      input_html_options[:data].merge!({
+        tiny_mce_editor: '',
+        content_css: template.asset_path('admin/tinymce-editor.css'),
+        media_handler: 'media-modal',
+        media_src: "file.large.url",
+        media_alt: "name",
+        media_url: "[src]",
+        convert_urls: false
+      })
+
       super
     end
   end
