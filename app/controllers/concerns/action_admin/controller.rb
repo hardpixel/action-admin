8894f8c569c4d8bad48b99ea06df2ab8d5831dac
Olibia Tsati <olibia@hardpixel.eu>
2017-12-21 17:33:59 +0200

    set admin locale

--- app/controllers/concerns/action_admin/controller.rb
+++ app/controllers/concerns/action_admin/controller.rb
@@ -9,11 +9,19 @@ module ActionAdmin
       # Use forgery protection
       protect_from_forgery with: :exception
 
+      # Set locale
+      before_action :set_admin_locale
+
       # Render default view
       rescue_from ActionController::UnknownFormat, with: :render_default_view
     end
 
     private
+
+      # Set admin default locale
+      def set_admin_locale
+        I18n.locale = ActionAdmin.config.admin_locale || I18n.default_locale
+      end
 
       # Render default view template
       def render_default_view
