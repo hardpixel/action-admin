36369d9219c0cb03053d0b5f0111ab7f45e8f5c2
Olibia Tsati <olibia@hardpixel.eu>
2017-12-25 03:40:39 +0200

    disable tinymce media button

--- app/inputs/action_admin/tinymce_input.rb
+++ app/inputs/action_admin/tinymce_input.rb
@@ -5,10 +5,10 @@ module ActionAdmin
       input_html_options[:data].merge!({
         tiny_mce_editor: '',
         content_css: template.asset_path('admin/tinymce-editor.css'),
-        media_handler: 'media-modal',
-        media_src: "file.large.url",
-        media_alt: "name",
-        media_url: "[src]",
+        # media_handler: 'media-modal',
+        # media_src: "file.large.url",
+        # media_alt: "name",
+        # media_url: "[src]",
         shortcode_handler: 'shortcode-modal',
         convert_urls: false
       })
