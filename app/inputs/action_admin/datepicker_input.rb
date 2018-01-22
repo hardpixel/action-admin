5e9e19356038efc69b472958828803254e7820fe
Jonian Guveli <jonian@hardpixel.eu>
2018-01-22 17:21:43 +0200

    update datepicker input

--- app/inputs/action_admin/datepicker_input.rb
+++ app/inputs/action_admin/datepicker_input.rb
@@ -1,6 +1,8 @@
 module ActionAdmin
   class DatepickerInput < SimpleForm::Inputs::StringInput
     def input(wrapper_options)
+      input_html_options[:type] ||= 'text'
+
       input_html_options[:data] ||= {}
       input_html_options[:data].merge!(date_picker: '')
 
