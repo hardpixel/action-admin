275fbc3e54ba3a65de4d6765071471845c3b4f5b
Jonian Guveli <jonian@hardpixel.eu>
2018-04-11 18:33:43 +0300

    update text area input

--- app/inputs/action_admin/textarea_input.rb
+++ app/inputs/action_admin/textarea_input.rb
@@ -2,10 +2,7 @@ module ActionAdmin
   class TextareaInput < SimpleForm::Inputs::TextInput
     def input(wrapper_options)
       input_html_options[:data] ||= {}
-      input_html_options[:data].merge!({
-        textarea: '',
-        resize_icon: 'mce-ico mce-i-resize'
-      })
+      input_html_options[:data].merge!({ textarea: '' })
 
       super
     end
