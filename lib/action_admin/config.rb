e9106b6862d9be4ed3eb3ff6b4eeea8bb45dbc60
Jonian Guveli <jonian@hardpixel.eu>
2019-01-05 13:08:53 +0200

    disable hashie warnings in menu and shortcodes configs

--- lib/action_admin/config.rb
+++ lib/action_admin/config.rb
@@ -1,11 +1,23 @@
 module ActionAdmin
+  class MenuConfig < Hashie::Mash
+    self.disable_warnings
+  end
+
+  class MenusConfig < Hashie::Mash
+    self.disable_warnings
+  end
+
+  class ShortcodesConfig < Hashie::Mash
+    self.disable_warnings
+  end
+
   class Config < Hashie::Dash
     property :app_name,          default: 'Action Admin'
     property :app_urls,          default: :web_url
     property :admin_locale,      default: :en
     property :admin_index_scope, default: nil
-    property :menus,             default: Hashie::Mash.new
-    property :shortcodes,        default: Hashie::Mash.new
+    property :menus,             default: MenusConfig.new
+    property :shortcodes,        default: ShortcodesConfig.new
     property :shortcode_helper,  default: :render_shortcode
     property :shortcode_layout,  default: 'shortcode'
     property :shortcode_assets,  default: ['application.css', 'application.js']
@@ -14,7 +26,7 @@ module ActionAdmin
 
     def menu(name, &block)
       if self.menus.send(:"#{name}").nil?
-        self.menus.send(:"#{name}=", Hashie::Mash.new)
+        self.menus.send(:"#{name}=", MenuConfig.new)
       end
 
       yield self.menus.send(:"#{name}")
